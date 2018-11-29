200dd8db936050a2b2c8c3b6507aaf0991102ee1
fbriers <fbriers@west.com>
2018-11-29 15:57:52 +0000

    Add project files.

---
+++ FriendlyBank/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FriendlyBank
+{
+    class Account
+    {
+        //Fields
+        public AccountState State;
+        public string Name;
+        public string Address;
+        public int AccountNumber;
+        private decimal Balance = 0;
+        public int Overdraft;
+
+        public static void PrintAccount(Account a)
+        {
+            WriteLine("Name: " + a.Name);
+            WriteLine("Address: " + a.Address);
+        }
+
+        public bool WithdrawFunds (decimal amount)
+        {
+            if (Balance < amount)
+            {
+                return false;
+            }
+            Balance = Balance - amount;
+            return true;
+        }
+        public enum AccountState
+        {
+            New,
+            Active,
+            UnderAudit,
+            Frozen,
+            Closed
+        };
+
+    }
+}
---
+++ FriendlyBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace FriendlyBank
+{
+    class Program
+    {
+      
+        public static void Main()
+        {
+            const int Max_Cust = 100;
+            Account[] Bank = new Account[Max_Cust];
+
+            Account TimsAccount = new Account();
+            TimsAccount.Name = "Tim";
+            if (TimsAccount.WithdrawFunds (4))
+            {
+                WriteLine("Cash Withdrawn");
+            }
+            else
+            {
+                WriteLine("Insufficient Funds");
+            }
+            WriteLine(TimsAccount.Name);
+            Read();
+        }
+    }
+}
